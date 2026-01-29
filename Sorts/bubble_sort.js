a329ca6758a433b96cb8f4801d186dd68f11454b
Gagik Setikyan <setikyang@gmail.com>
2026-01-29 16:48:50 +0400

    task

--- Sorts/bubble_sort.js
+++ Sorts/bubble_sort.js
@@ -1,11 +1,15 @@
 function Buble_Sort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let l = 0; l < arr.length - 1 - i; l++) {
-      if (arr[l] >= arr[l + 1]) {
-        [arr[l], arr[l + 1]] = [arr[l + 1], arr[l]]
+  let n = arr.length;
+  let flag;
+  do {
+    flag = false;
+    for (let j = 0; j < n; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        flag = true;
       }
     }
-  }
+  } while (flag);
   return arr;
 }
 
