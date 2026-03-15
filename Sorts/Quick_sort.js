c2ed3e604ccb1e627aa9dcd0fb36205c31419b3c
Gagik Setikyan <setikyang@gmail.com>
2026-03-15 12:55:49 +0400

    task

--- Sorts/Quick_sort.js
+++ Sorts/Quick_sort.js
@@ -2,11 +2,14 @@ function partition(arr, low, high) {
   let pivot = arr[low];
   let i = low + 1;
   let j = high;
+
   while (i <= j) {
-    while (arr[i] < pivot) i++;
+    while (i <= high && arr[i] < pivot) i++;
     while (arr[j] > pivot) j--;
     if (i <= j) {
       [arr[i], arr[j]] = [arr[j], arr[i]];
+      i++;
+      j--;
     }
   }
   [arr[low], arr[j]] = [arr[j], arr[low]];
@@ -15,14 +18,13 @@ function partition(arr, low, high) {
 
 function Quick(arr, low = 0, high = arr.length - 1) {
   if (low < high) {
-    let par = partition(arr, low, high);
-    Quick(arr, low, par);
-    Quick(arr, par + 1, high);
+    let p = partition(arr, low, high);
+    Quick(arr, low, p - 1);
+    Quick(arr, p + 1, high);
   }
 }
 
 let arr = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 Quick(arr);
 console.log(arr);
-
 module.exports = Quick;
